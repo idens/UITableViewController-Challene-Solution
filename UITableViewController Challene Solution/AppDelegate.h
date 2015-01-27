5a53238086613a77a1da1d1def54dba0bc466265
iDens <denis.roks@gmail.com>
2015-01-27 23:48:30 +0300

    Initial Commit

---
+++ UITableViewController Challene Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challene Solution
+//
+//  Created by Admin on 27.01.15.
+//  Copyright (c) 2015 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challene Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challene Solution
+//
+//  Created by Admin on 27.01.15.
+//  Copyright (c) 2015 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
